63a3e2453e4ce23e6617d8b0f59b4e625dfee5b8
statxanalytics-commits <statxanalytics@gmail.com>
2026-06-18 13:25:35 +0200

    Update diet.tsx

--- app/(app)/diet.tsx
+++ app/(app)/diet.tsx
@@ -195,11 +195,6 @@ export default function DietScreen() {
             <iframe
               src={webUrl}
               style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, width: '100%', height: '100%', border: 'none' } as any}
-              <iframe
-  src={webUrl}
-  style={{ position: 'absolute', ... }}
-
-/>
             />
           </View>
         ) : (
